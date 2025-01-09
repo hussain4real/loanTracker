ddb1b3e5a4871a4f8aaaba7c07940ec1ceea32eb
Hussain Aminu <hussain_aminu@yahoo.com>
2025-01-09 11:23:25 +0300

    Enhance LoanStatus enum to include getColorClasses method for dynamic color styling; update loan view to utilize color classes for status display.

--- app/Enums/LoanStatus.php
+++ app/Enums/LoanStatus.php
@@ -33,4 +33,37 @@ enum LoanStatus: string implements HasColor, HasLabel
             self::DEFAULTED => 'danger',
         };
     }
+
+    //
+    public function getColorClasses(): array
+    {
+        $color = match ($this->getColor()) {
+            'gray' => [
+                'text' => 'text-gray-700 dark:text-gray-300',
+                'bg' => 'bg-gray-100 dark:bg-gray-900',
+            ],
+            Color::Blue => [
+                'text' => 'text-blue-700 dark:text-blue-300',
+                'bg' => 'bg-blue-100 dark:bg-blue-900',
+            ],
+            'danger' => [
+                'text' => 'text-red-700 dark:text-red-300',
+                'bg' => 'bg-red-100 dark:bg-red-900',
+            ],
+            Color::Green => [
+                'text' => 'text-green-700 dark:text-green-300',
+                'bg' => 'bg-green-100 dark:bg-green-900',
+            ],
+            'warning' => [
+                'text' => 'text-yellow-700 dark:text-yellow-300',
+                'bg' => 'bg-yellow-100 dark:bg-yellow-900',
+            ],
+            default => [
+                'text' => 'text-gray-700 dark:text-gray-300',
+                'bg' => 'bg-gray-100 dark:bg-gray-900',
+            ],
+        };
+
+        return $color;
+    }
 }
